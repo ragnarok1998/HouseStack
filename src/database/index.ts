3c9f5a0ede7cbf69b182d21edde2572430a07cd2
gourav chandna <gourav@myanatomy.in>
2020-11-20 05:35:00 +0530

    server part done

---
+++ src/database/index.ts
@@ -0,0 +1,20 @@
+require("dotenv").config({
+  path: "/home/myanatomy/Documents/HouseStack/src/.env",
+});
+
+import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+console.log(url);
+
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("main");
+  return {
+    listings: db.collection("test_listings"),
+  };
+};
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+require("dotenv").config({
+  path: "/home/myanatomy/Documents/HouseStack/src/.env",
+});
+
+import express, { Application } from "express";
+import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database";
+import { resolvers, typeDefs } from "./graphql";
+
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+  server.applyMiddleware({ app, path: "/api" });
+
+  app.listen(process.env.PORT);
+
+  console.log(`[app]:http://localhost:${process.env.PORT}`);
+  // const listings = await db.listings.find({}).toArray();
+  // console.log(listings);
+};
+
+mount(express());
